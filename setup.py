3334bd567742fda30766295b932792e6bfb9ebc0
Mykhailo-Musiienko <mykhailo.musiienko@nure.ua>
2021-11-23 08:55:27 +0200

    SRS

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup,find_packages
 
 setup(
-   name='department-app',
-   description='Module web department-app',
+   name='teachers_unievrsities',
+   description='Module web teachers_unievrsities',
    author='Mykhailo Musiienko',
    author_email='mykhailo.musiienko@nure.ua',
    packages=find_packages(),
